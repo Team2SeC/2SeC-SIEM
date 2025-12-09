2bcc8a91da687a150d5b1fca89f142a2f3f0ff06
hej <hejwien@gmail.com>
2025-12-09 11:50:26 +0900

    chore: ec2 보안그룹 설정에서 불필요한 443 포트제외

--- infra/dev/modules/ec2/main.tf
+++ infra/dev/modules/ec2/main.tf
@@ -24,20 +24,10 @@ resource "aws_security_group" "web" {
   description = "Security group for DVWA Web Server"
   vpc_id      = var.vpc_id
 
-  # HTTP
   ingress {
     description = "HTTP"
     from_port   = 80
     to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # HTTPS
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }

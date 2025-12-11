a6519a2763f86089e608e05b994363a2c888472d
youngwon <youngwon@youngwonui-MacBookPro.local>
2025-12-11 16:14:30 +0900

    add bootstrap ecr, only logstash

---
+++ bootstrap/ecr/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS 리전을 설정합니다 (예: ap-northeast-2)."
+  default     = "ap-northeast-2"
+}
+
+variable "project_name" {
+  type        = string
+  description = "프로젝트 공통 prefix (예: 2SeC)."
+}
+
+variable "environment" {
+  type        = string
+  description = "환경 구분 값 (예: dev, prod)."
+  default     = "dev"
+}
+
+

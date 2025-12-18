996408f9c065dfc2268455f8749c85b0c9efbe31
Heo-Ye-Eun <huone0329@gmail.com>
2025-12-18 17:31:43 +0900

    refactor: 2sec->2SeC

--- infra/dev/variables.tf
+++ infra/dev/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "project_name" {
   type        = string
   description = "프로젝트 공통 prefix (예: project02)."
-  default     = "2sec"    # 대문자때문에 에러가 나는 것 같아 수정
+  default     = "2SeC"   
 }
 
 variable "environment" {
